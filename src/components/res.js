7cb7257f87d2255d50c058492a28bc5a063b14cf
Michelle Dcruz <52381792+michelledcruz@users.noreply.github.com>
2023-07-09 22:41:55 +1000

    Update res.js

--- src/components/res.js
+++ src/components/res.js
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react";
 import {useParams} from "react-router-dom";
  
 const RestaurantMenu = () => {
+
+  const {resId} = useParams();
+
+  const {res, setRes} = useState();
+  
+  useEffect(() => {
+    getRestuarantInfo();
+  }, [])
+
+  async function getRestuarantInfo(){
+    const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.0759837&lng=72.8776559&restaurantId=103810");
+    const json = await data.json();
+    console.log(json);
+  }
 
   const {id} = useParams();
     return (
